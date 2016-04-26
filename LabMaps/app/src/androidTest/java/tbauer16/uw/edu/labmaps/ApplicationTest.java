d2b72150088d43619e80f2b28eb3e44735d8c3a0
tbauer516 <tcbauer516@gmail.com>
2016-04-26 10:13:21 -0700

    added demo code from lab

---
+++ LabMaps/app/src/androidTest/java/tbauer16/uw/edu/labmaps/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tbauer16.uw.edu.labmaps;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
